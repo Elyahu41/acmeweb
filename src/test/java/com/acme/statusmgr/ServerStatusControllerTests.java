94e27278a8cc7ea2500379fc8378d588754950c4
Ely <Elyahu41@gmail.com>
2020-11-15 23:54:15 -0500

    Added JUnit Tests

--- src/test/java/com/acme/statusmgr/ServerStatusControllerTests.java
+++ src/test/java/com/acme/statusmgr/ServerStatusControllerTests.java
@@ -15,6 +15,7 @@
  */
 package com.acme.statusmgr;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -38,17 +39,71 @@ public class ServerStatusControllerTests {
 
     @Test
     public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
         this.mockMvc.perform(get("/server/status")).andDo(print()).andExpect(status().isOk())
                 .andExpect(jsonPath("$.statusDesc").value("Server is up"));
     }
 
     @Test
     public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-
         this.mockMvc.perform(get("/server/status").param("name", "RebYid"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(jsonPath("$.contentHeader").value("Server Status requested by RebYid"));
     }
 
+    @Test
+    public void withParamShouldReturnTailored_Name_Basic_Op() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed?details=operations"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, and is operating normally"));
+    }
+
+    @Test
+    public void withParamShouldReturnTailored_Basic_Op_Ext() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed?details=operations,extensions"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, and is operating normally," +
+                        " and is using these extensions - [Hypervisor, Kubernetes, RAID-6]"));
+    }
+
+    @Test
+    public void withParamShouldReturnTailored_Basic_Op_Ext_Mem() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed?details=operations,extensions,memory"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, and is operating normally," +
+                        " and is using these extensions - [Hypervisor, Kubernetes, RAID-6], and its memory is running low"));
+    }
+
+    @Test
+    public void withParamShouldReturnTailored_Name_Basic_Op_Ext_Mem() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed?details=operations,extensions,memory&name=Noach"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.contentHeader").value("Server Status requested by Noach"))
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, and is operating normally" +
+                        ", and is using these extensions - [Hypervisor, Kubernetes, RAID-6]"+", and its memory is running low"));
+    }
+
+    @Test
+    public void withParamShouldReturnTailored_Name_Basic_Mem_Op() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed?details=memory,operations&name=yaakov"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.contentHeader").value("Server Status requested by yaakov"))
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, and its memory is running low," +
+                        " and is operating normally"));
+    }
+
+    @Test
+    public void withParamShouldReturnTailored_Basic_Op_Mem_Ext() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed?details=operations,memory,extensions"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, and is operating normally" +
+                        ", and its memory is running low, and is using these extensions - [Hypervisor, Kubernetes, RAID-6]"));
+    }
+
+    @Test
+    public void withJunkErrorParameterShouldReturnException() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed?details=memory,operations,junkERROR"))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(status().reason(is("Double check your details parameters")));
+
+    }
 }
